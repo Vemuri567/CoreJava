ec7bd399f0629282af41370da089f3632e472879
prasannasamineni <162083638+prasannasamineni@users.noreply.github.com>
2024-03-27 10:20:26 +0530

    Create AvgOf100nos.java
    error occured

---
+++ src/Conditions/AvgOf100nos.java
@@ -0,0 +1,31 @@
+package Conditions;
+
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class AvgOf100nos {
+    public static void Avg () {
+
+
+        for (int i = 1; i <= 100; i++) {
+            int a = i + 1;
+
+
+            System.out.println(a);
+
+            int b = int i *a;
+
+            int c = b / 2;
+
+
+            System.out.println(c);
+        }
+        }
+
+
+
+    public static void main(String[] args) {
+
+        Avg();
+
+    }
+}
