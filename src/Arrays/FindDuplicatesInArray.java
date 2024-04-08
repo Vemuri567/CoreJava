3e760f854f75a2c449a7382cbc5b63464adfc497
NAVEEN <naveenchowdaryvemuri@gmail.com>
2024-04-08 09:53:26 +0530

    Printing Duplicate Array

--- src/Arrays/FindDuplicatesInArray.java
+++ src/Arrays/FindDuplicatesInArray.java
@@ -9,7 +9,7 @@ public class FindDuplicatesInArray {
 
             for(int i=0;i< a.length;i++){
                 for(int j=i+1;j<a.length;j++){
-                    if(a[i]==a[j]){
+                    if(a[i]!=a[j]){
                         int count=0;
                         for(int n=0;n<duplicateArray.length;n++){
                             if(a[i]==duplicateArray[n]){
