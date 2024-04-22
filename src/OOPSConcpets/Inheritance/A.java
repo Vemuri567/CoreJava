b4f9ad03b59c56c82218e2e5e70b0066eccacf95
NAVEEN <naveenchowdaryvemuri@gmail.com>
2024-04-22 11:38:20 +0530

    Inheritance

---
+++ src/OOPSConcpets/Inheritance/A.java
@@ -0,0 +1,19 @@
+package OOPSConcpets.Inheritance;
+
+public class A {
+    static int c=10;
+    int a;
+ public A(int a)
+ {
+     this.a=a;
+ }
+    public void ParentBalance()
+    {
+        System.out.println("Parent balance");
+    }
+
+    public static void ParentBalance1()
+    {
+        System.out.println("Parent balance");
+    }
+}
---
+++ src/OOPSConcpets/Inheritance/Main.java
@@ -0,0 +1,22 @@
+package OOPSConcpets.Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        /*B.c=30;
+        B.d=40;
+        System.out.println(B.c);
+        System.out.println(B.d);*/
+        B obj=new B(10);
+        obj.ParentBalance();
+        obj.ChildBlanace();
+        obj.ChildBlanace1();
+        obj.a=10;
+        obj.b=20;
+        /*A obj1=new A();
+        obj1.ParentBalance();
+        obj1.a=10;
+        A obj2=new B();
+        obj2.ParentBalance();
+        obj2.a=10;*/
+    }
+}
