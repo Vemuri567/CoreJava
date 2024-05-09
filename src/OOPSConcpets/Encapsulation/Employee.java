48c00c64a4bbd493b2d72ce4c97ed605d4c41d9a
NAVEEN <naveenchowdaryvemuri@gmail.com>
2024-05-09 20:42:42 +0530

    Encapsulation

---
+++ src/OOPSConcpets/Encapsulation/Employee.java
@@ -0,0 +1,38 @@
+package OOPSConcpets.Encapsulation;
+
+public class Employee
+{
+    private String empAge;
+    private String empId;
+    private String empName;
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+
+
+    public String getEmpAge() {
+        return empAge;
+    }
+
+    public void setEmpAge(String empAge) {
+        this.empAge = empAge;
+    }
+
+
+
+}
---
+++ src/OOPSConcpets/Encapsulation/Main.java
@@ -0,0 +1,10 @@
+package OOPSConcpets.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp=new Employee();
+        emp.setEmpId("120s");
+        String empid=emp.getEmpId();
+        System.out.println(empId);
+    }
+}
